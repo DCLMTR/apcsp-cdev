4239c93880360b1ef7765ddc878a8f57da4d0613
Kyle Yin Xu <yinxuk2619@my.sduhsd.net>
2021-01-20 08:42:45 +0000

    loadstudent and encrypt to do

--- proj/student.c
+++ proj/student.c
@@ -21,15 +21,30 @@ Student* students[100] = {0};
 
 void createStudent(char* fname, char* lname, int age, int id)
 {
+  Student* st = (Student*)malloc(sizeof(Student));
+  char* firstname = (char*)malloc(50*sizeof(fname));
+  firstname = fname;
+  char* lastname = (char*)malloc(50*sizeof(lname));
+  lastname = lname;
+  st->firstName = firstname;
+  st->lastName = lastname;
+  st->age = age;
+  st->id = id;
+  students[numStudents] = st;
+  numStudents++;
   // createStudent dynamically creates a Student on the heap and adds that
   // student to the student array
   //  - the firstName and lastName strings should be dynamically created
   //    based on the size of the fname and lname args
+
+
 }
 
 
 void deleteStudent(Student* student)
 {
+  free(student->firstName);
+  free(student->lastName);
   free(student);
   // free the memory associated with a student including the strings
 }
@@ -39,11 +54,26 @@ void deleteStudents()
 {
   // iterate over the students array deleting every student and setting te pointer
   // values to 0 and adjusting the numStudents to 0
+  for(int i = 0; i < numStudents; i++){
+    deleteStudent(students[i]);
+    students[i]->firstName = 0;
+    students[i]->lastName = 0;
+  }
+  numStudents = 0;
 }
 
 
 void saveStudents(int key)
 {
+FILE* fp;
+fp = fopen(STUFILE, "w");
+if (fp) 
+{
+  for(int i = 0; i < numStudents; i++)
+    //add encryption for key later students[i].fname+=key;
+    fprintf(fp, "%s %s %d %lu\n", students[i]->firstName, students[i]->lastName, students[i]->age, students[i]->id);
+  fclose(fp);
+}
   // save all students in the student array to a file 'studentdata.txt' overwriting
   // any existing file
   //   - the format of the file is one line per student as follows fname lname age id:
@@ -56,6 +86,32 @@ void saveStudents(int key)
 void loadStudents(int key)
 {
   // load the students from the data file overwriting all exisiting students in memory
+  FILE* fp;
+  fp = fopen("studentdata.txt", "r");
+  if (fp) 
+  {
+    numStudents = 0;
+    while (1)
+    {
+      char* fname = (char*)malloc(50 * sizeof(char)); char* lname = (char*)malloc(50 * sizeof(char)); int age; long unsigned int id;
+      if (fscanf(fp, "%s %s %d %lu\n", fname, lname, &age, &id) == 2)
+      {
+        //printf("test");
+        //students[numStudents]->firstName = fname;
+        //students[numStudents]->lastName = lname;
+        //students[numStudents]->age = age;
+        //students[numStudents]->id = id;
+        //numStudents++;
+      }
+      else
+      {
+        free(fname);
+        free(lname);
+        break;
+      }
+      fclose(fp);
+    }
+  }
 }
 
 
